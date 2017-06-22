0d831066864871db1ac7ca1a526ba9aeaf2d182e
Cristian Banarescu <cristianbanarescu@gmail.com>
2017-06-22 11:29:14 +0300

    Add new Joke : -can add new joke category using a text field and a button -created an editable text view for the new joke description

--- Jokes/Jokes/UI/AddNewJokeViewController.swift
+++ Jokes/Jokes/UI/AddNewJokeViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class AddNewJokeViewController: UIViewController {
+class AddNewJokeViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+  
 
+    @IBOutlet weak var jokePickerView: UIPickerView!
+//    @IBOutlet weak var jokeCategoryLabel: UILabel!
+    @IBOutlet weak var jokeDescription: UITextView!
+    @IBOutlet weak var newCategoryTextField: UITextField!
+    
+    var pickerData : [String] = [String] ()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerData = ["category1","category2","category3"]
      
         // Do any additional setup after loading the view.
     }
@@ -28,16 +38,42 @@ class AddNewJokeViewController: UIViewController {
         
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveAddJoke(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        return
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       
+//        if let newCategory = newCategoryTextField.text{
+//            pickerData[row] = newCategory
+//        }
+        
+        return pickerData[row]
+    }
+    
+    
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    
+    @IBAction func addNewCategory(_ sender: UIButton) {
+        if let newCategory = newCategoryTextField.text{
+            pickerData.append(newCategory)
+            jokePickerView.reloadAllComponents()
+        }
+    }
+
+    
 
 }
