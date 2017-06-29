93ccd2a34412adbbaa76fca5e3c934c0d0f7c360
Irina Țari <irinatari95@gmail.com>
2017-06-29 18:25:26 +0300

    AddImageController: URL works

--- Jokes/Jokes/UI/AddImageViewController.swift
+++ Jokes/Jokes/UI/AddImageViewController.swift
@@ -26,13 +26,27 @@ class AddImageViewController: UIViewController {
     
     @IBAction func saveImage(_ sender: Any) {
         
-        dismiss(animated: true, completion: nil)
+        if  let urlPath = URL(string: urlTextField.text!) {
+        
+        downloadImage(url: urlPath)
+            
+        } else {
+            let alertView = UIAlertController.init(title: "Error", message: "Add viable URL before saving", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertView.addAction(cancelAction)
+            present(alertView, animated: true)
+            
+        }
+        
+       // dismiss(animated: true, completion: nil)
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        urlTextField.isUserInteractionEnabled = true
+        imageNameTextField.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
     }
 
@@ -40,6 +54,39 @@ class AddImageViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // Get data from URL
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    // Download image
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else {
+                let alert = UIAlertController(title: "Unsafe URL", message: "The URL path you have chose does not start with HTTPS and it is not safe. Please choose an image with HTTPS URL prefix", preferredStyle: .alert)
+                
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alert.addAction(cancelAction)
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.urlImageView.image = UIImage(data: data)
+            }
+        }
+    }
+
+    
     
 
     /*
