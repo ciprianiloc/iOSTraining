6ee19e54e15067a272c3aefc1e8e50781d9f072f
Cristian Banarescu <cristianbanarescu@gmail.com>
2017-07-17 13:14:56 +0300

    no message

--- Jokes/Jokes/UI/AllJokesTableViewController.swift
+++ Jokes/Jokes/UI/AllJokesTableViewController.swift
@@ -175,6 +175,7 @@ class AllJokesTableViewController: UITableViewController {
     
     
     
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailView = storyboard?.instantiateViewController(withIdentifier: "DetailJoke") as? DetailJokeViewController
         
